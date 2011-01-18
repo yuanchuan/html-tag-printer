22e989ca0a321f73fd041b8a7541faf5c0c3dd2b
spirit23 <spirit23@163.com>
2011-01-18 15:58:13 +0800

    formatting and correct a bad reg expression

--- html-tag-printer.php
+++ html-tag-printer.php
@@ -9,7 +9,7 @@
    * @license: MIT License
    */
    
-  function T(){
+  function T() {
     $args = func_get_args();
 
     /*
@@ -29,11 +29,11 @@
     /*
      * A semi-closed tag has a different endtag and no midtag.
      */
-    $semi_closed_pattern="/input|img|area|base|col|hr|br|meta|param/";
+    $semi_closed_pattern = "/^(input|img|area|base|col|hr|br|meta|param)/";
     
     
-    if(func_num_args() && is_string($args[0])){
-      $tag=trim($args[0]);
+    if ( func_num_args() && is_string($args[0]) ) {
+      $tag = trim($args[0]);
       /*
        * TODO: Detect invalid tags
        */ 
@@ -41,63 +41,55 @@
       $html['midtag'] = preg_match($semi_closed_pattern,$tag) ?  '' : '>';
       $html['endtag'] = empty($html['midtag']) ? ' />' : '</'.$tag.'>';
       
-      if(isset($args[1])){
-        $arg1=$args[1];
+      if ( isset($args[1]) ) {
+        $arg1 = $args[1];
         
         /*
          * $args[1] is a text node if it is of string type.
          */
-        if(is_string($arg1)){
-          /*
-           * TODO: Convert special characters (<>'&) to HTML entity equivalent.
-           */
-          $html['text']=$arg1;
+        if ( is_string($arg1) ) {
+          $html['text'] = $arg1;
         }
         
         /*
          * There are two situations of $args[1] to be an array:
          * 
          * 1.It is an array of attributes;
-         * 2.It is an array of embeded self function calls and returns
-         * child element tags.
+         * 2.It is an array of embeded self function calls which returns
+         *   child element tags.
          * 
          * The distinction is that the two arrays have different type of array
-         * keys.In the first case the type of the keys is string and in the second
-         * the type of keys is number.
+         * keys. In the first case the type of the keys is string and in the second
+         * the type is number.
          */
-        
-        if(is_array($arg1)){
+        if ( is_array($arg1) ) {
           foreach($arg1 as $key => $value){
-            $value=trim($value);
+            $value = trim($value);
             
-            if(is_string($key)){
+            if ( is_string($key) ) {
               /*
                * An array of attributes.
                */
-              $html['attr'].=empty($value) ? '' : (' '.$key.'="'.$value.'"');
+              $html['attr'] .= empty($value) ? '' : (' '.$key.'="'.$value.'"');
               
-            }else{
+            } else {
               /*
                * An array of embeded functions which returns string.
                */
-              $html['text'].=$value;
+              $html['text'] .= $value;
             }
           }
                    
-          if(isset($args[2])){
-            $arg2=$args[2];
+          if ( isset($args[2]) ) {
+            $arg2 = $args[2];
             
             /*
              * $args[2] is text node whether it's a string or an array of strings.
              */
-            if(is_string($arg2)){
-              /*
-               * TODO: Convert special characters (<>'&) to HTML entity equivalent.
-               */
-              $html['text'].=$arg2;
-              
-            }else if(is_array($arg2)){
-              $html['text'].=join('',$arg2);
+            if( is_string($arg2) ) {
+              $html['text'] .= $arg2;  
+            } else if ( is_array($arg2) ) {
+              $html['text'] .= join('',$arg2);
             }
           }
         }
